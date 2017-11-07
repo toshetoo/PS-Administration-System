79558870e941b0986fe7e3e187239750e8a03c41
Todor <todor.ivanov@primeholding.com>
2017-11-07 17:05:35 +0200

    Add validation for the author form

--- src/components/authors/AuthorForm.js
+++ src/components/authors/AuthorForm.js
@@ -8,15 +8,17 @@ class AuthorForm extends React.Component {
             <form onSubmit={this.props.onSave}>
                 <h1>Manage Author</h1>
                 <TextInput label='First Name'
-                       name='firstName'
-                       value={this.props.author.firstName}
-                       onChange={this.props.onChange}/>
+                           name='firstName'
+                           value={this.props.author.firstName}
+                           onChange={this.props.onChange}
+                           error={this.props.errors.firstName}/>
                 <br/>
                 <label htmlFor='lastName'>Last name</label>
                 <TextInput label='Last Name'
                            name='lastName'
                            value={this.props.author.lastName}
-                           onChange={this.props.onChange}/>
+                           onChange={this.props.onChange}
+                           error={this.props.errors.lastName}/>
                 <br/>
                 <input type='submit' value='Save' className='btn btn-primary'/>
             </form>
--- src/components/authors/ManageAuthor.js
+++ src/components/authors/ManageAuthor.js
@@ -7,33 +7,77 @@ let toastr = require('toastr');
 
 class ManageAuthor extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            author: {id: '', firstName: '', lastName: ''}
+    static willTransitionFrom(transition, component) {
+        if(component.state.dirty && !confirm('Leave without saving?')) {
+            transition.abort();
         }
     }
 
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            author: {id: '', firstName: '', lastName: ''},
+            errors: {},
+            dirty: false
+        };
+    }
+
     setAuthorState(event) {
+        this.setState({
+            dirty: true
+        });
+
         this.state.author[event.target.name] = event.target.value;
         this.setState({
-            author: this.state.author
-        })
+            author: this.state.author,
+            dirty: true
+        });
     }
 
     saveAuthor(event) {
         event.preventDefault();
+        if (!this.authorFormIsValid()) {
+            return;
+        }
+
         AuthorAPI.saveAuthor(this.state.author);
         toastr.success('Author added successfully!');
+        this.setState({
+            dirty: false
+        });
         Router.History.back();
     }
+
+    authorFormIsValid() {
+        let isValid = true;
+        this.state.errors = {};
+
+        if (this.state.author.firstName.length < 3) {
+            isValid = false;
+            this.state.errors.firstName = 'Too short name';
+        }
+
+        if (this.state.author.lastName.length < 3) {
+            isValid = false;
+            this.state.errors.lastName = 'Too short name';
+        }
+
+        this.setState({
+            errors: this.state.errors
+        });
+
+        return isValid;
+    }
+
 
     render() {
         return (
             <div>
                 <AuthorForm author={this.state.author}
                             onChange={this.setAuthorState.bind(this)}
-                            onSave={this.saveAuthor.bind(this)}/>
+                            onSave={this.saveAuthor.bind(this)}
+                            errors={this.state.errors}/>
             </div>
         );
     }
